89bf89922419baf8104dd6474908754e09fba757
Sergio González Sánchez <soysergiogonzalez@outlook.es>
2021-10-22 15:32:55 -0500

    Se suben archivos

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "./components/Title";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Hola desde Login js</h1>
+      <Title text="Soy un titulo con props" />
+    </div>
+  );
+};
+
+export default Login;
